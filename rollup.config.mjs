5f55b19ed65f1445029c1f2fea84e1c163a0cabe
jkh675 <rluo2018@gmail.com>
2024-08-19 18:59:29 +0800

    Migrate to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,26 @@
+import typescript from "@rollup/plugin-typescript";
+import path from "path";
+
+const OUTPUT_FOLDER = path.join(process.cwd(), "dist");
+const LIB_NAME = "jsez"
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+	input: "src/index.ts",
+	output: [
+		{
+			file: path.join(OUTPUT_FOLDER, "index.cjs"),
+			format: "cjs",
+		},
+		{
+			file: path.join(OUTPUT_FOLDER, "index.mjs"),
+			format: "esm",
+		},
+		{
+			file: path.join(OUTPUT_FOLDER, "index.umd.mcs"),
+			format: "umd",
+			name: LIB_NAME
+		},
+	],
+	plugins: [typescript()],
+};
